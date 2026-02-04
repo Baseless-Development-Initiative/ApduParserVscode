db1711b59c1cc3e5490b76c7103ab8e16e9de30b
Chinmaya Acharya <chinmaya.acharya_1@nxp.com>
2026-02-04 14:49:56 +0530

    Testing

---
+++ scripts/run.bat
@@ -0,0 +1,4 @@
+@REM npm install
+@REM npm run compile
+
+@REM npm i --save-dev esbuild
--- src/apdu_defs.ts
+++ src/apdu_defs.ts
@@ -33,11 +33,17 @@ interface P2 {
     name:string;
 }
 
+interface TLV {
+    tag:number;
+    name:string;
+}
+
 interface INS {
     val:number;
     name:string;
     p1:P1[];
     p2:P2[];
+    tlvs:TLV[];
 }
 
 interface APDUDef {
@@ -51,6 +57,12 @@ export interface APDU {
     p1:P1;
     p2:P2;
 }
+
+// interface AnotherAPDUINS {
+//     name:string;
+//     apdu_id:number;
+//     tlvs:TLV[];
+// }
 
 const PATH_TO_CONFIG_FILE:string = "/.vscode/apdu_config.json";
 
@@ -81,108 +93,282 @@ function resyncApduConfig(path:string):string {
     return file
 }
 
-function ApduUpdaterP1P2(object:any):P1[]|P2[]|undefined {
-    let p1p2:P1[]|P2[] = [];
+// function ApduUpdaterP1P2(object:any):P1[]|P2[]|undefined {
+//     let p1p2:P1[]|P2[] = [];
 
+//     for (let entry of object) {
+//         let name:string = "";
+//         let val:number;
+//         Object.entries(entry).forEach(([key, value]) => {
+//             if ("Name" === key) {
+//                 name = <string> value;
+//             }
+//             else if ("val" === key) {
+//                 val = parseInt(<string> value, 16);
+//                 let param:P1 = {name:name, val:val};
+//                 p1p2.push(param);
+//             }
+//         })
+//     }
+//     if (p1p2.length === 0) {
+//         return undefined;
+//     }
+
+//     return p1p2;
+// }
+
+// function ApduUpdaterINS(object:any):INS[] {
+//     let ins_array:INS[] = [];
+//     // let ins_array = new Map<number, string>();
+//     Object.entries(object).forEach(([ins, value]) => {
+//         let ins_obj:INS = {
+//             name: "",
+//             p1:[{name:P1_Default_str,val:P1_Default}],
+//             p2:[{name:P2_Default_str,val:P2_Default}],
+//             val:0
+//         };
+//         let value_json:JSON = <JSON> value;
+//         Object.entries(value_json).forEach(([key2, value2]) => {
+//             if ("Name" === key2) {
+//                 ins_obj.val = parseInt(ins, 16);
+//                 ins_obj.name = value2;
+//             }
+//             else if ("P1" === key2) {
+//                 let p1 = ApduUpdaterP1P2(value2);
+//                 if (p1) {
+//                     ins_obj.p1 = p1;
+//                 }
+//             }
+//             else if ("P2" === key2) {
+//                 let p2 = ApduUpdaterP1P2(value2);
+//                 if (p2) {
+//                     ins_obj.p2 = p2;
+//                 }
+//             }
+//             else {
+//                 /** Add other parameter handling here, eg for P1/P2 */
+//                 vscode.window.showWarningMessage("Unknown parameter ", key2);
+//             }
+//         })
+//         ins_array.push(ins_obj);
+//     })
+//     return ins_array;
+// }
+
+// function ApduUpdaterCLA(object:any)
+// {
+//     Object.entries(object).forEach(([cla, value]) => {
+//         let cla_value:number = parseInt(cla, 16);
+//         let value_json:JSON = <JSON> value;
+//         Object.entries(value_json).forEach(([key, value2]) => {
+//             if (key === "Name") {
+//                 /** Found the CLA string - add entry */
+//                 if (0x00 === cla_value) {
+//                     /** Interindustry APDU */
+//                     interindustry_commands.cla.val = cla_value;
+//                     interindustry_commands.cla.name = value2;
+//                 }
+//                 else {
+//                     /** Proprietary APDU */
+//                     proprietary_commands.cla.val = cla_value;
+//                     proprietary_commands.cla.name = value2;
+//                 }
+//             }
+//             else if (key === "INS") {
+//                 if (0x00 === cla_value) {
+//                     /** Add INS values to interindustry commands */
+//                     let ins:INS[] = ApduUpdaterINS(value2);
+//                     for (let entry of ins) {
+//                         interindustry_commands.ins.push(entry);
+//                     }
+//                 }
+//                 else {
+//                     /** Add INS values to proprietary commands */
+//                     let ins:INS[] = ApduUpdaterINS(value2);
+//                     for (let entry of ins) {
+//                         proprietary_commands.ins.push(entry);
+//                     }
+//                 }
+//             }
+//             else {
+//                 vscode.window.showErrorMessage("Unknown key in JSON file");
+//                 return;
+//             }
+//         })
+//     })
+// }
+
+function isP1InINS(ins:INS, p1:P1):boolean {
+    for (let p1entry of ins.p1) {
+        if (p1entry.val === p1.val) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function isP2InINS(ins:INS, p2:P2):boolean {
+    for (let p2entry of ins.p2) {
+        if (p2entry.val === p2.val) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function isTLVInEntry(ins:INS, tlv:TLV):boolean {
+    for (let tlventry of ins.tlvs) {
+        if (tlventry.tag === tlv.tag) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function ParseApduP1(object:any):P1[] {
+    let p1_array:P1[] = [];
     for (let entry of object) {
-        let name:string = "";
-        let val:number;
+        let name:string|undefined = undefined;
+        let val:number|undefined = undefined;
         Object.entries(entry).forEach(([key, value]) => {
-            if ("Name" === key) {
+            if ("name" === key) {
                 name = <string> value;
             }
             else if ("val" === key) {
                 val = parseInt(<string> value, 16);
-                let param:P1 = {name:name, val:val};
-                p1p2.push(param);
             }
         })
+        if (!(undefined === name || undefined === val)) {
+            let p1:P1 = {name:name, val:val};
+            p1_array.push(p1);
+        }
+        else {
+            vscode.window.showWarningMessage("Corrupted JSON file: parsing P1");
+        }
     }
-    if (p1p2.length === 0) {
-        return undefined;
-    }
-
-    return p1p2;
+    return p1_array;
 }
 
-function ApduUpdaterINS(object:any):INS[] {
-    let ins_array:INS[] = [];
-    // let ins_array = new Map<number, string>();
-    Object.entries(object).forEach(([ins, value]) => {
-        let ins_obj:INS = {
-            name: "",
-            p1:[{name:P1_Default_str,val:P1_Default}],
-            p2:[{name:P2_Default_str,val:P2_Default}],
-            val:0
-        };
-        let value_json:JSON = <JSON> value;
-        Object.entries(value_json).forEach(([key2, value2]) => {
-            if ("Name" === key2) {
-                ins_obj.val = parseInt(ins, 16);
-                ins_obj.name = value2;
+function ParseApduP2(object:any):P2[] {
+    let p2_array:P2[] = [];
+    for (let entry of object) {
+        let name:string|undefined = undefined;
+        let val:number|undefined = undefined;
+        Object.entries(entry).forEach(([key, value]) => {
+            if ("name" === key) {
+                name = <string> value;
             }
-            else if ("P1" === key2) {
-                let p1 = ApduUpdaterP1P2(value2);
-                if (p1) {
-                    ins_obj.p1 = p1;
-                }
-            }
-            else if ("P2" === key2) {
-                let p2 = ApduUpdaterP1P2(value2);
-                if (p2) {
-                    ins_obj.p2 = p2;
-                }
-            }
-            else {
-                /** Add other parameter handling here, eg for P1/P2 */
-                vscode.window.showWarningMessage("Unknown parameter ", key2);
+            else if ("val" === key) {
+                val = parseInt(<string> value, 16);
             }
         })
-        ins_array.push(ins_obj);
-    })
-    return ins_array;
+        if (!(undefined === name || undefined === val)) {
+            let p2:P2 = {name:name, val:val};
+            p2_array.push(p2);
+        }
+        else {
+            vscode.window.showWarningMessage("Corrupted JSON file: parsing P2");
+        }
+    }
+    return p2_array;
 }
 
-function ApduUpdaterCLA(object:any)
+function ParseApduTLVs(object:any):TLV[] {
+    let tlv_array:TLV[] = [];
+    for (let entry of object) {
+        let name:string|undefined = undefined;
+        let val:number|undefined = undefined;
+        Object.entries(entry).forEach(([key, value]) => {
+            if ("name" === key) {
+                name = <string> value;
+            }
+            else if ("val" === key) {
+                val = parseInt(<string> value, 16);
+            }
+        })
+        if (!(undefined === name || undefined === val)) {
+            let tlv:TLV = {name:name, tag:val};
+            tlv_array.push(tlv);
+        }
+        else {
+            vscode.window.showWarningMessage("Corrupted JSON file: parsing P2");
+        }
+    }
+    return tlv_array;
+}
+
+function AddEntryIntoAPDUClass(commands:APDUDef, name:string, ins_value:number, p1:P1[], p2:P2[], tlvs:TLV[])
 {
-    Object.entries(object).forEach(([cla, value]) => {
-        let cla_value:number = parseInt(cla, 16);
-        let value_json:JSON = <JSON> value;
-        Object.entries(value_json).forEach(([key, value2]) => {
-            if (key === "Name") {
-                /** Found the CLA string - add entry */
-                if (0x00 === cla_value) {
-                    /** Interindustry APDU */
-                    interindustry_commands.cla.val = cla_value;
-                    interindustry_commands.cla.name = value2;
-                }
-                else {
-                    /** Proprietary APDU */
-                    proprietary_commands.cla.val = cla_value;
-                    proprietary_commands.cla.name = value2;
+    // Look for entry in proprietary array
+    let added:boolean = false;
+    for (let entry of commands.ins) {
+        if (entry.name === name) {
+            // Found INS entry
+            added = true;
+            for (let p1entry of p1) {
+                if (false === isP1InINS(entry, p1entry)) {
+                    entry.p1.push(p1entry);
                 }
             }
-            else if (key === "INS") {
-                if (0x00 === cla_value) {
-                    /** Add INS values to interindustry commands */
-                    let ins:INS[] = ApduUpdaterINS(value2);
-                    for (let entry of ins) {
-                        interindustry_commands.ins.push(entry);
-                    }
-                }
-                else {
-                    /** Add INS values to proprietary commands */
-                    let ins:INS[] = ApduUpdaterINS(value2);
-                    for (let entry of ins) {
-                        proprietary_commands.ins.push(entry);
-                    }
+            for (let p2entry of p2) {
+                if (false === isP2InINS(entry, p2entry)) {
+                    entry.p2.push(p2entry);
                 }
             }
-            else {
-                vscode.window.showErrorMessage("Unknown key in JSON file");
-                return;
+            for (let tlventry of tlvs) {
+                if (false === isTLVInEntry(entry, tlventry)) {
+                    entry.tlvs.push(tlventry);
+                }
+            }
+        }
+    }
+    if (false === added) {
+        // No entry found matching the INS name
+        // Create a new entry for INS
+        let ins:INS = {name:name, val:ins_value, p1:p1, p2:p2, tlvs:tlvs};
+        commands.ins.push(ins);
+    }
+}
+
+function ParseApduConfig(object:any)
+{
+    Object.entries(object).forEach(([name, apdu]) => {
+        let apdu_json:JSON = <JSON> apdu;
+        let cla_value:number|undefined = undefined;
+        let ins_value:number|undefined = undefined;
+        let p1:P1[] = [{name: P1_Default_str, val: P1_Default}];
+        let p2:P2[] = [{name: P2_Default_str, val: P2_Default}];
+        let tlvs:TLV[] = [];
+        Object.entries(apdu_json).forEach(([key, value]) => {
+            if ("CLA" === key) {
+                // Found class key
+                cla_value = parseInt(value, 16);
+            }
+            else if ("INS" === key) {
+                ins_value = parseInt(value, 16);
+            }
+            else if ("P1" === key) {
+                p1 = ParseApduP1(value);
+            }
+            else if ("P2" === key) {
+                p2 = ParseApduP2(value);
+            }
+            else if ("TLV" === key) {
+                tlvs = ParseApduTLVs(value);
             }
         })
+        if (cla_value && ins_value) {
+            // Look for existing entry in array
+            if (cla_value & CLA_Proprietary) {
+                AddEntryIntoAPDUClass(proprietary_commands, name, ins_value, p1, p2, tlvs);
+            }
+            else {
+                AddEntryIntoAPDUClass(interindustry_commands, name, ins_value, p1, p2, tlvs);
+            }
+        }
+        else {
+            vscode.window.showWarningMessage("Incorrect entry");
+        }
     })
 }
 
@@ -198,11 +384,12 @@ export async function ApduListUpdate()
             let parsed_json = JSON.parse(text);
             // let yy = parsed_json['CLA'];
 
-            Object.entries(parsed_json).forEach(([key, value]) => {
-                if ('CLA' === key) {
-                    ApduUpdaterCLA(value);
-                }
-            })
+            ParseApduConfig(parsed_json);
+            // Object.entries(parsed_json).forEach(([key, value]) => {
+            //     if ('CLA' === key) {
+            //         ApduUpdaterCLA(value);
+            //     }
+            // })
 
             // let doc = vscode.workspace.openTextDocument({content:text});
             // vscode.window.showTextDocument(await doc);
@@ -221,7 +408,7 @@ export function ParseAPDUHeader(cla:number, ins:number, p1:number, p2:number):AP
 {
     let apdu:APDU = {
         cla:{name:"Unknown", val:cla},
-        ins:{name:"Unknown", val:ins, p1:[{name:"", val:0x00}], p2:[{name:"", val:0x00}]},
+        ins:{name:"Unknown", val:ins, p1:[{name:"", val:0x00}], p2:[{name:"", val:0x00}], tlvs:[]},
         p1:{name:"Unknown", val:p1},
         p2:{name:"Unknown", val:p2},
     }
@@ -235,42 +422,54 @@ export function ParseAPDUHeader(cla:number, ins:number, p1:number, p2:number):AP
     if ((CLA_Proprietary & cla) == CLA_Proprietary) {
         /** Proprietary APDU */
         apdu.cla.name = proprietary_commands.cla.name;
-        for (let entry of proprietary_commands.ins) {
-            if (entry.val === ins) {
-                /** Entry exists */
-                apdu.ins.name = entry.name;
-                for (let p1_entry of entry.p1) {
+        for (let ins_entry of proprietary_commands.ins) {
+            let found_match:boolean = false;
+            if (ins_entry.val === ins) {
+                for (let p1_entry of ins_entry.p1) {
                     if (p1_entry.val === p1) {
-                        apdu.p1.name = p1_entry.name;
+                        for (let p2_entry of ins_entry.p2) {
+                            if (p2_entry.val === p2) {
+                                // Found match
+                                found_match = true;
+                                apdu.ins.name = ins_entry.name;
+                                apdu.p1.name = p1_entry.name;
+                                apdu.p2.name = p2_entry.name;
+                                break;
+                            }
+                        }
+                        break;
                     }
                 }
-                for (let p2_entry of entry.p2) {
-                    if (p2_entry.val === p2) {
-                        apdu.p2.name = p2_entry.name;
-                    }
+                if (found_match) {
+                    break;
                 }
-                break;
             }
         }
     }
     else {
         /** Interindustry APDU */
         apdu.cla.name = interindustry_commands.cla.name;
-        for (let entry of interindustry_commands.ins) {
-            if (entry.val === ins) {
-                /** Entry exists */
-                apdu.ins.name = entry.name;
-                for (let p1_entry of entry.p1) {
+        for (let ins_entry of interindustry_commands.ins) {
+            let found_match:boolean = false;
+            if (ins_entry.val === ins) {
+                for (let p1_entry of ins_entry.p1) {
                     if (p1_entry.val === p1) {
-                        apdu.p1.name = p1_entry.name;
+                        for (let p2_entry of ins_entry.p2) {
+                            if (p2_entry.val === p2) {
+                                // Found match
+                                found_match = true;
+                                apdu.ins.name = ins_entry.name;
+                                apdu.p1.name = p1_entry.name;
+                                apdu.p2.name = p2_entry.name;
+                                break;
+                            }
+                        }
+                        break;
                     }
                 }
-                for (let p2_entry of entry.p2) {
-                    if (p2_entry.val === p2) {
-                        apdu.p2.name = p2_entry.name;
-                    }
+                if (found_match) {
+                    break;
                 }
-                break;
             }
         }
     }
